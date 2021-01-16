f477bd459935341d30947b5fbec9c16707d8fa7f
Damir Medakovic <damir0096@hotmail.com>
2021-01-16 15:00:50 +0100

    added readme photos

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -13,13 +13,11 @@ export default function Home() {
       <NavbarHome />
       <FormHero />
 
-      <div class="px-4 grid grid-cols-3 gap-4 mt-20 shadow-2xl border-b-2 border-gray-300 text-center h-screen rounded-full bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
-    </div>
       <div class="flex justify-center mt-16">
       <div class="text-center rounded-3xl shadow-2xl border-2 border-gray-300 w-4/5">
         <div class="pt-20 text-4xl font-medium">
           <p>
-            Markedsplassen for genuine posts som promoterer{" "}
+            Genuine posts som promoterer{" "}
             <span class="text-indigo-800">din</span> merkevare.
           </p>
         </div>
